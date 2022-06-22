d9af2bbfbac77d329147df8ff0697fe61e85d57d
dzomboallan <dzomboallan@gmail.com>
2022-06-23 00:59:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tablayout/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.tablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.getFragment(new Fragment1(),"CHATS");
+        vpAdapter.getFragment(new Fragment4(),"GROUPS");
+        vpAdapter.getFragment(new Fragment2(),"STATUS");
+        vpAdapter.getFragment(new Fragment3(),"CALLS");
+        viewPager.setAdapter(vpAdapter);
+    }
+}
